51838d27d30715cfc7c331e9ac4e33d40bf56af7
Becca <becca@adadevelopersacademy.org>
2021-02-04 17:30:32 -0800

    long description

--- swap_meet/decor.py
+++ swap_meet/decor.py
@@ -7,3 +7,13 @@ class Decor(Item):
 
     def __str__(self):
         return "Something to decorate your space."
+
+    def long_description(self):
+        if self.condition > 4.0:
+            print("Very good condition")
+        elif 3.0 < self.condition <= 4.0:
+            print("Pretty good condition")
+        elif  2.0 < self.condition <= 3.0:
+            print("Noticeable wear and tear")
+        else:
+            print("Fashionably rustic")
--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,6 +1,10 @@
 class Item:
-    def __init__(self, category=''):
+    def __init__(self, category='', condition=0):
         self.category = category
+        self.condition = condition
 
     def __str__(self):
         return "Hello World!"
+
+    def longest_description(self):
+        return "implemented in child"
